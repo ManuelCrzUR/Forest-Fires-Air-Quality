d4b96f8a95520fc25b333f62f35200076e46a6ad
Manuel Cruz Garrote <manuels.cruz@urosario.edu.co>
2025-03-27 10:16:51 -0500

    inserción de datos en las tablas de las bases de datos

--- Tables_setup.sql
+++ Tables_setup.sql
@@ -1,4 +1,3 @@
--- CREACIÓN DE TODAS LAS TABLAS QUE SON NECESARIAS (PAISES E INCENDIOS FORESETALES)
 
 -- Tablas para Brasil 2010
 CREATE TABLE fire_data_2010_Brazil (
@@ -17,152 +16,45 @@ CREATE TABLE fire_data_2010_Brazil (
     frp FLOAT,
     daynight CHAR(1),
     type INT
-);
-
--- Tablas para Brasil
-CREATE TABLE fire_data_2011_Brazil (LIKE fire_data_2010_Brazil INCLUDING ALL);
-CREATE TABLE fire_data_2012_Brazil (LIKE fire_data_2010_Brazil INCLUDING ALL);
-CREATE TABLE fire_data_2013_Brazil (LIKE fire_data_2010_Brazil INCLUDING ALL);
-CREATE TABLE fire_data_2014_Brazil (LIKE fire_data_2010_Brazil INCLUDING ALL);
-CREATE TABLE fire_data_2015_Brazil (LIKE fire_data_2010_Brazil INCLUDING ALL);
-CREATE TABLE fire_data_2016_Brazil (LIKE fire_data_2010_Brazil INCLUDING ALL);
-CREATE TABLE fire_data_2017_Brazil (LIKE fire_data_2010_Brazil INCLUDING ALL);
-CREATE TABLE fire_data_2018_Brazil (LIKE fire_data_2010_Brazil INCLUDING ALL);
-CREATE TABLE fire_data_2019_Brazil (LIKE fire_data_2010_Brazil INCLUDING ALL);
-CREATE TABLE fire_data_2020_Brazil (LIKE fire_data_2010_Brazil INCLUDING ALL);
-
--- Tablas para Chile
-CREATE TABLE fire_data_2010_Chile (LIKE fire_data_2010_Brazil INCLUDING ALL);
-CREATE TABLE fire_data_2011_Chile (LIKE fire_data_2010_Brazil INCLUDING ALL);
-CREATE TABLE fire_data_2012_Chile (LIKE fire_data_2010_Brazil INCLUDING ALL);
-CREATE TABLE fire_data_2013_Chile (LIKE fire_data_2010_Brazil INCLUDING ALL);
-CREATE TABLE fire_data_2014_Chile (LIKE fire_data_2010_Brazil INCLUDING ALL);
-CREATE TABLE fire_data_2015_Chile (LIKE fire_data_2010_Brazil INCLUDING ALL);
-CREATE TABLE fire_data_2016_Chile (LIKE fire_data_2010_Brazil INCLUDING ALL);
-CREATE TABLE fire_data_2017_Chile (LIKE fire_data_2010_Brazil INCLUDING ALL);
-CREATE TABLE fire_data_2018_Chile (LIKE fire_data_2010_Brazil INCLUDING ALL);
-CREATE TABLE fire_data_2019_Chile (LIKE fire_data_2010_Brazil INCLUDING ALL);
-CREATE TABLE fire_data_2020_Chile (LIKE fire_data_2010_Brazil INCLUDING ALL);
-
--- Tablas para Colombia
-CREATE TABLE fire_data_2010_Colombia (LIKE fire_data_2010_Brazil INCLUDING ALL);
-CREATE TABLE fire_data_2011_Colombia (LIKE fire_data_2010_Brazil INCLUDING ALL);
-CREATE TABLE fire_data_2012_Colombia (LIKE fire_data_2010_Brazil INCLUDING ALL);
-CREATE TABLE fire_data_2013_Colombia (LIKE fire_data_2010_Brazil INCLUDING ALL);
-CREATE TABLE fire_data_2014_Colombia (LIKE fire_data_2010_Brazil INCLUDING ALL);
-CREATE TABLE fire_data_2015_Colombia (LIKE fire_data_2010_Brazil INCLUDING ALL);
-CREATE TABLE fire_data_2016_Colombia (LIKE fire_data_2010_Brazil INCLUDING ALL);
-CREATE TABLE fire_data_2017_Colombia (LIKE fire_data_2010_Brazil INCLUDING ALL);
-CREATE TABLE fire_data_2018_Colombia (LIKE fire_data_2010_Brazil INCLUDING ALL);
-CREATE TABLE fire_data_2019_Colombia (LIKE fire_data_2010_Brazil INCLUDING ALL);
-CREATE TABLE fire_data_2020_Colombia (LIKE fire_data_2010_Brazil INCLUDING ALL);
 
 
+-- Crear tablas para Brasil
+CREATE TABLE modis_2010_Brazil (LIKE fire_data_2010_Brazil INCLUDING ALL);
+CREATE TABLE modis_2011_Brazil (LIKE fire_data_2010_Brazil INCLUDING ALL);
+CREATE TABLE modis_2012_Brazil (LIKE fire_data_2010_Brazil INCLUDING ALL);
+CREATE TABLE modis_2013_Brazil (LIKE fire_data_2010_Brazil INCLUDING ALL);
+CREATE TABLE modis_2014_Brazil (LIKE fire_data_2010_Brazil INCLUDING ALL);
+CREATE TABLE modis_2015_Brazil (LIKE fire_data_2010_Brazil INCLUDING ALL);
+CREATE TABLE modis_2016_Brazil (LIKE fire_data_2010_Brazil INCLUDING ALL);
+CREATE TABLE modis_2017_Brazil (LIKE fire_data_2010_Brazil INCLUDING ALL);
+CREATE TABLE modis_2018_Brazil (LIKE fire_data_2010_Brazil INCLUDING ALL);
+CREATE TABLE modis_2019_Brazil (LIKE fire_data_2010_Brazil INCLUDING ALL);
+CREATE TABLE modis_2020_Brazil (LIKE fire_data_2010_Brazil INCLUDING ALL);
 
--- TABLAS POR AÑO
--- Tabla general por año
-CREATE TABLE fire_data_2010 AS
-SELECT * FROM fire_data_2010_Brazil
-UNION ALL
-SELECT * FROM fire_data_2010_Chile
-UNION ALL
-SELECT * FROM fire_data_2010_Colombia;
+-- Crear tablas para Colombia
+CREATE TABLE modis_2010_Colombia (LIKE fire_data_2010_Brazil INCLUDING ALL);
+CREATE TABLE modis_2011_Colombia (LIKE fire_data_2010_Brazil INCLUDING ALL);
+CREATE TABLE modis_2012_Colombia (LIKE fire_data_2010_Brazil INCLUDING ALL);
+CREATE TABLE modis_2013_Colombia (LIKE fire_data_2010_Brazil INCLUDING ALL);
+CREATE TABLE modis_2014_Colombia (LIKE fire_data_2010_Brazil INCLUDING ALL);
+CREATE TABLE modis_2015_Colombia (LIKE fire_data_2010_Brazil INCLUDING ALL);
+CREATE TABLE modis_2016_Colombia (LIKE fire_data_2010_Brazil INCLUDING ALL);
+CREATE TABLE modis_2017_Colombia (LIKE fire_data_2010_Brazil INCLUDING ALL);
+CREATE TABLE modis_2018_Colombia (LIKE fire_data_2010_Brazil INCLUDING ALL);
+CREATE TABLE modis_2019_Colombia (LIKE fire_data_2010_Brazil INCLUDING ALL);
+CREATE TABLE modis_2020_Colombia (LIKE fire_data_2010_Brazil INCLUDING ALL);
 
-CREATE TABLE fire_data_2011 AS
-SELECT * FROM fire_data_2011_Brazil
-UNION ALL
-SELECT * FROM fire_data_2011_Chile
-UNION ALL
-SELECT * FROM fire_data_2011_Colombia;
+-- Crear tablas para Chile
+CREATE TABLE modis_2010_Chile (LIKE fire_data_2010_Brazil INCLUDING ALL);
+CREATE TABLE modis_2011_Chile (LIKE fire_data_2010_Brazil INCLUDING ALL);
+CREATE TABLE modis_2012_Chile (LIKE fire_data_2010_Brazil INCLUDING ALL);
+CREATE TABLE modis_2013_Chile (LIKE fire_data_2010_Brazil INCLUDING ALL);
+CREATE TABLE modis_2014_Chile (LIKE fire_data_2010_Brazil INCLUDING ALL);
+CREATE TABLE modis_2015_Chile (LIKE fire_data_2010_Brazil INCLUDING ALL);
+CREATE TABLE modis_2016_Chile (LIKE fire_data_2010_Brazil INCLUDING ALL);
+CREATE TABLE modis_2017_Chile (LIKE fire_data_2010_Brazil INCLUDING ALL);
+CREATE TABLE modis_2018_Chile (LIKE fire_data_2010_Brazil INCLUDING ALL);
+CREATE TABLE modis_2019_Chile (LIKE fire_data_2010_Brazil INCLUDING ALL);
+CREATE TABLE modis_2020_Chile (LIKE fire_data_2010_Brazil INCLUDING ALL);
 
-CREATE TABLE fire_data_2012 AS
-SELECT * FROM fire_data_2012_Brazil
-UNION ALL
-SELECT * FROM fire_data_2012_Chile
-UNION ALL
-SELECT * FROM fire_data_2012_Colombia;
-
-CREATE TABLE fire_data_2013 AS
-SELECT * FROM fire_data_2013_Brazil
-UNION ALL
-SELECT * FROM fire_data_2013_Chile
-UNION ALL
-SELECT * FROM fire_data_2013_Colombia;
-
-CREATE TABLE fire_data_2014 AS
-SELECT * FROM fire_data_2014_Brazil
-UNION ALL
-SELECT * FROM fire_data_2014_Chile
-UNION ALL
-SELECT * FROM fire_data_2014_Colombia;
-
-CREATE TABLE fire_data_2015 AS
-SELECT * FROM fire_data_2015_Brazil
-UNION ALL
-SELECT * FROM fire_data_2015_Chile
-UNION ALL
-SELECT * FROM fire_data_2015_Colombia;
-
-CREATE TABLE fire_data_2016 AS
-SELECT * FROM fire_data_2016_Brazil
-UNION ALL
-SELECT * FROM fire_data_2016_Chile
-UNION ALL
-SELECT * FROM fire_data_2016_Colombia;
-
-CREATE TABLE fire_data_2017 AS
-SELECT * FROM fire_data_2017_Brazil
-UNION ALL
-SELECT * FROM fire_data_2017_Chile
-UNION ALL
-SELECT * FROM fire_data_2017_Colombia;
-
-CREATE TABLE fire_data_2018 AS
-SELECT * FROM fire_data_2018_Brazil
-UNION ALL
-SELECT * FROM fire_data_2018_Chile
-UNION ALL
-SELECT * FROM fire_data_2018_Colombia;
-
-CREATE TABLE fire_data_2019 AS
-SELECT * FROM fire_data_2019_Brazil
-UNION ALL
-SELECT * FROM fire_data_2019_Chile
-UNION ALL
-SELECT * FROM fire_data_2019_Colombia;
-
-CREATE TABLE fire_data_2020 AS
-SELECT * FROM fire_data_2020_Brazil
-UNION ALL
-SELECT * FROM fire_data_2020_Chile
-UNION ALL
-SELECT * FROM fire_data_2020_Colombia;
-
--- CRACIÓN DE TABLA GENERA UNIFICADA
-
-CREATE TABLE fire_data AS
-SELECT * FROM fire_data_2010
-UNION ALL
-SELECT * FROM fire_data_2011
-UNION ALL
-SELECT * FROM fire_data_2012
-UNION ALL
-SELECT * FROM fire_data_2013
-UNION ALL
-SELECT * FROM fire_data_2014
-UNION ALL
-SELECT * FROM fire_data_2015
-UNION ALL
-SELECT * FROM fire_data_2016
-UNION ALL
-SELECT * FROM fire_data_2017
-UNION ALL
-SELECT * FROM fire_data_2018
-UNION ALL
-SELECT * FROM fire_data_2019
-UNION ALL
-SELECT * FROM fire_data_2020;
-
--- Consulta de ususario para conexión de python:
-
-select current_user,now()
+	
--- conexion_carga.py
+++ conexion_carga.py
@@ -10,7 +10,7 @@ try:
        user="postgres",
        password="123456789" #poner contraseña propia
    )
-    print('Conexión exitosa')
+    print('Conexión exitosa' + "\n")
     
     cursor = connection.cursor()
     
@@ -21,10 +21,78 @@ try:
     for table in tables:
         print(table[0])
         
+    print("\n")
+        
     # Contar tablas en la base de datos
     cursor.execute("SELECT COUNT(*) FROM information_schema.tables WHERE table_schema='public'")
     table_count = cursor.fetchone()[0]
-    print(f"Cantidad de tablas en la base de datos: {table_count}")
+    print(f"Cantidad de tablas en la base de datos: {table_count}" + "\n")
     
-except:
-    print('Error de conexión')
+    # Cargar datos desde multiples archivos CSV a la base de datos
+    
+    # ruta CSV
+    ruta_csv = "C:/Users/manue/Desktop/work/INS_AQ&FF/data/forest_fires"
+    
+    # nombres de los archivos CSV
+    archivos = ["modis_2010_Brazil", "modis_2011_Brazil", "modis_2012_Brazil", 
+                "modis_2013_Brazil", "modis_2014_Brazil", "modis_2015_Brazil",
+                "modis_2016_Brazil", "modis_2017_Brazil", "modis_2018_Brazil",
+                "modis_2019_Brazil", "modis_2020_Brazil", "modis_2010_Colombia",
+                "modis_2011_Colombia", "modis_2012_Colombia", "modis_2013_Colombia",
+                "modis_2014_Colombia", "modis_2015_Colombia", "modis_2016_Colombia",
+                "modis_2017_Colombia", "modis_2018_Colombia", "modis_2019_Colombia",
+                "modis_2020_Colombia", "modis_2010_Chile", "modis_2011_Chile",
+                "modis_2012_Chile", "modis_2013_Chile", "modis_2014_Chile",
+                "modis_2015_Chile", "modis_2016_Chile", "modis_2017_Chile",
+                "modis_2018_Chile", "modis_2019_Chile", "modis_2020_Chile"]
+    
+    # columnas de las tablas
+    columnas = ['latitude', 'longitude', "brightness", "scan", "track", 
+    "acq_date", "acq_time", "satellite", "instrument", "confidence", 
+    "version", "bright_t31", "frp", "daynight", "type"]
+    
+    # Consultar el primer dato de la tabla modis_2020_colombia
+    print("Verificar que existan datos en DB para no añadirlos de nuevo" + "\n")
+    cursor.execute("SELECT * FROM modis_2020_colombia LIMIT 1")
+    first_row = cursor.fetchone()  # Obtener la primera fila
+
+    if first_row:
+        print("Primer dato de la tabla modis_2020_colombia:", first_row)
+    else:
+        print("La tabla está vacía")
+        c = 0
+        for tabla in tables[1:]:
+            t = tabla[0].lower()
+            a = archivos[c].lower()
+
+            
+            if t == a:
+                print(f"Cargando datos en la tabla: {t}")
+
+                # Cargar datos desde el archivo CSV
+                archivo_csv = f"{ruta_csv}/{archivos[c]}.csv"
+                df = pd.read_csv(archivo_csv, usecols=columnas)
+                
+                # Insertar datos en la tabla
+                insert_query = f"INSERT INTO {t} ({', '.join(columnas)}) VALUES ({', '.join(['%s'] * len(columnas))})"
+                cursor.executemany(insert_query, df.values.tolist())
+
+                # Confirmar cambios en la base de datos
+                connection.commit()
+                print(f"Datos insertados en la tabla: {t}")
+
+                c += 1
+            else:
+                print("diferentes")
+                print(f"Error: {a} no coincide con {t}")
+                break
+
+except Exception as e:
+    print(f"Error: {e}")
+
+finally:
+    if connection:
+        cursor.close()
+        connection.close()
+        print("Conexión cerrada")
+        
